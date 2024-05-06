d02e4b06de1f4ad0c806b6b22aa117a3fa6fb148
akshadamodak06 <161637747+akshadamodak06@users.noreply.github.com>
2024-05-06 18:25:05 +0530

    Create do_while_loop.cpp

---
+++ Chapter4/do_while_loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main()
+{
+    const int count{10};
+    size_t i{0};
+    do//in this loop the 
+    {
+        std::cout<<"The value of i is "<<i<<" and I Love C++"<<std::endl;
+        i++;
+    } while (i<10);
+    return 0;
+}
