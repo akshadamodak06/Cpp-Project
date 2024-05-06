1b28e256e46242ff1acb6afcbc2841e6b197b24f
akshadamodak06 <161637747+akshadamodak06@users.noreply.github.com>
2024-05-06 15:26:13 +0530

    Update Basic_input.cpp

--- Chapter1/Basic_input.cpp
+++ Chapter1/Basic_input.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+int main()
+{
+    int a;
+    std::cout<<"Enter your Id: ";
+    std::cin>>a;
+    std::cout<<"Your Id is: "<<a<<std::endl;
+    std::string name;
+    std::cout<<"Write you First name: "<<name;
+    std::cin>>name;
+    std::cout<<"Your name is "<<name<<std::endl;
+    //prints name20
+    return 0;
 
+}
