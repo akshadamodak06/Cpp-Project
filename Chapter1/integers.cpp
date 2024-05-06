1f9dede0c6ab13fe99d7c31c286ea344226729cb
akshadamodak06 <161637747+akshadamodak06@users.noreply.github.com>
2024-05-06 15:35:51 +0530

    Update integers.cpp

--- Chapter1/integers.cpp
+++ Chapter1/integers.cpp
@@ -1,1 +1,10 @@
-
+#include <iostream>
+int main()
+{
+    int x(5.3);
+   // int y{5.3};
+    int z=5.3;
+    std::cout<<"The number is: "<<x<<std::endl;
+   // std::cout<<"The number is: "<<y<<std::endl;
+    std::cout<<"The number is: "<<z<<std::endl;
+}
