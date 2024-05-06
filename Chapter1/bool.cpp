0a42cdca73dcadf1e2f123ab3b9ff950bf646fc3
akshadamodak06 <161637747+akshadamodak06@users.noreply.github.com>
2024-05-06 15:33:17 +0530

    Update bool.cpp

--- Chapter1/bool.cpp
+++ Chapter1/bool.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+int main()
+{
+    bool red {false};
+    bool green {true};
 
+    if(red==true)
+    {
+        std::cout << "Stop" <<std::endl;
+    }
+    else
+    {
+        std::cout<<"Go through!!"<<std::endl;
+    }
+
+    if(green)
+    {
+        std::cout<<"The light is green: "<<std::endl;
+    }
+    else
+    {
+        std::cout<<"The light is NOT green"<<std::endl;
+    }
+
+    std::cout<<"The red light is: "<<red<<std::endl;
+    std::cout<<"The green light is: "<<green<<std::endl;
+    std::cout<<"Size of bool is : "<<sizeof(red)<<std::endl;
+
+    std::cout<<std::boolalpha;
+    std::cout<<"The red light is: "<<red<<std::endl;
+    std::cout<<"The green light is: "<<green<<std::endl;
+
+    return 0;
+}
