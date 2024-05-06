199c901e4de40c262ccb5cfaeba437e55d9ba023
akshadamodak06 <161637747+akshadamodak06@users.noreply.github.com>
2024-05-06 15:32:04 +0530

    Update Sum.cpp

--- Chapter1/Sum.cpp
+++ Chapter1/Sum.cpp
@@ -1,1 +1,12 @@
+#include<iostream>
+int main()
+{
+    int sum;
+    int first_number {13};
+    int second_number {7};
+    sum=first_number+second_number;
+    std::cout<<"First Number: "<<first_number<<std::endl<<"Second Number:"<<second_number<<std::endl;
+    std::cout<<"Sum: "<<sum;
+    return 0;
+}
 
