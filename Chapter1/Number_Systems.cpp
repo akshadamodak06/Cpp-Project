76eb7801daf2855022f6ac5b96274d46d11a8588
akshadamodak06 <161637747+akshadamodak06@users.noreply.github.com>
2024-05-06 15:31:20 +0530

    Update Number_Systems.cpp

--- Chapter1/Number_Systems.cpp
+++ Chapter1/Number_Systems.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+int main()
+{
+    int number1 {15};
+    int number2 = 017;//recognises the number as octal and stores it as integer  
+    int number3 =0x0f;//recognises the number as hexadecimal and stores it as integer 
+    int number4= 0b00001111;//recognises the number as binary and stores it as integer
 
+    std::cout<<"Then number is "<<number1<<std::endl;
+    std::cout<<"Then number is "<<number2<<std::endl;
+    std::cout<<"Then number is "<<number3<<std::endl;
+    std::cout<<"Then number is "<<number4<<std::endl;
+
+    return 0;
+}
