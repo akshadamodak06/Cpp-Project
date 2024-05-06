3aadc9f862b1ba7e5c2cb46d98f504fd1a6c7de0
akshadamodak06 <161637747+akshadamodak06@users.noreply.github.com>
2024-05-06 15:34:10 +0530

    Update char.cpp

--- Chapter1/char.cpp
+++ Chapter1/char.cpp
@@ -1,1 +1,10 @@
-
+#include<iostream>
+int main()
+{
+    char ch {'a'};
+    char value=65;
+    std::cout<< "Value: " << ch << std::endl;
+    std::cout<<"Value: "<<static_cast<int>(ch)<<std::endl;
+    std::cout<<"Value: "<<value<<std::endl;
+    std::cout<<"Value(int) :"<<static_cast<int>(value) <<std::endl;
+}
