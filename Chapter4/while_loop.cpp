815d9e2d383b29e59e6ce14746cce742da93ba5c
akshadamodak06 <161637747+akshadamodak06@users.noreply.github.com>
2024-05-06 18:26:18 +0530

    Create while_loop.cpp

---
+++ Chapter4/while_loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main()
+{
+    size_t i{0};
+    const size_t count{10};
+    while(i<count)
+    {
+        std::cout<<i<<" : I Love C++"<<std::endl;
+        i++;
+    }
+
+    std::cout<<"Loop Done"<<std::endl;
+    return 0;
+    
+}
+
+    
