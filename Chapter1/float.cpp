c298c3e4f9208c16614701e4c41e3c75b6e25f77
akshadamodak06 <161637747+akshadamodak06@users.noreply.github.com>
2024-05-06 15:35:15 +0530

    Update float.cpp

--- Chapter1/float.cpp
+++ Chapter1/float.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    float number1 {1.12345678901234567890};
+    double number2 {1.12345678901234567890};
+    long double number3 {1.12345678901234567890};
+    float x=0;
+    float y=10;
+
+    std::cout<<std::setprecision(20);
+    std::cout<<"First number: " <<number1<<std::endl;//precision 7 digits
+    std::cout<<"Second number: " <<number2<<std::endl;//precision 15 digits
+    std::cout<<"Third number: " <<number3<<std::endl;//precision 15+ digits
+    std::cout<<"The number is :"<<(y/x)<<std::endl;
+    std::cout<<"The number is :"<<(x/x);
+
+
+    return 0;
+}
 
