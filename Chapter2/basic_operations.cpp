c89a56e6cbbc4102bb4fe5b8707d63d6bda20387
akshadamodak06 <161637747+akshadamodak06@users.noreply.github.com>
2024-05-06 15:37:49 +0530

    Create basic_operations.cpp

---
+++ Chapter2/basic_operations.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main()
+{
+    int num1;
+    int num2;
+    std::cout<<"Enter the numbers: ";
+    std::cin>>num1>>num2;
+    std::cout<<" The sum is:"<<num1+num2<<std::endl;
+    std::cout<<" The diifference is:"<<num1-num2<<std::endl;
+    std::cout<<" The product is:"<<num1*num2<<std::endl;
+    std::cout<<" The quotient  is:"<<num1/num2<<std::endl;
+    std::cout<<"The remainder is: "<<num1%num2<<std::endl;
+        return 0;
+}
